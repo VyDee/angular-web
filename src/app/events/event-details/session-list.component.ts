b01b4bf3187b813e7fdc84309a7884597b742c61
VyDee <vy.duong@cuw.edu>
2020-05-25 16:25:39 -0500

    filtering data in the session-list

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnChanges } from '@angular/core'
 import { ISession } from '../shared'
 
 @Component({
@@ -6,6 +6,30 @@ import { ISession } from '../shared'
     templateUrl:'./session-list.component.html'
 })
 
-export class SessionListComponent {
-    @Input() sessions:ISession[]
+export class SessionListComponent implements OnChanges {
+    @Input() sessions:ISession[];
+    @Input() filterBy: string;
+    visibleSessions: ISession[] = [];
+
+    //OnChanges is called when the input in this class gets a new value
+    ngOnChanges() {
+        if(this.sessions)
+        {
+            this.filterSessions(this.filterBy);
+        }
+    }
+
+    filterSessions(filter) {
+        if(filter === 'all')
+        {
+            this.visibleSessions = this.sessions.slice(0);
+            //The .slice method is used to clone the entire session array and set it equals to the visibleSessions
+            //Duplicate the array with all the same element
+        }
+        else {
+            this.visibleSessions = this.sessions.filter(session => {
+                return session.level.toLocaleLowerCase() === filter;
+            })
+        }
+    }
 }
