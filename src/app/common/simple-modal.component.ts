d72791959fb241040265f5518b74092a49ebe19f
VyDee <vy.duong@cuw.edu>
2020-05-30 23:29:17 -0500

    ViewChild example

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ViewChild, ElementRef, Inject } from "@angular/core";
+import { JQ_TOKEN } from './jQuery.service';
 
 @Component({
     selector: 'simple-modal',
     template: `
-    <div id="{{elementId}}" class="modal fade" tabindex="-1">
+    <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -12,7 +13,7 @@ import { Component, Input } from "@angular/core";
                     </button>
                     <h4 class="modal-title">{{title}}</h4>
                 </div>
-                <div class="modal.body">
+                <div class="modal-body" (click)="closeModal()">
                     <ng-content></ng-content>
                 </div>
             </div>
@@ -27,4 +28,12 @@ import { Component, Input } from "@angular/core";
 export class SimpleModalComponent {
     @Input() title: string;
     @Input() elementId: string;
+    @ViewChild('modalcontainer') containerEl: ElementRef;
+
+    constructor(@Inject(JQ_TOKEN) private $: any){
+    }
+
+    closeModal(){
+        this.$(this.containerEl.nativeElement).modal('hide');
+    }
 }
