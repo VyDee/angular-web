4741fb9fb9e82ebe5bc7730c74d84ba1ffbb7d15
VyDee <vy.duong@cuw.edu>
2020-06-07 16:48:25 -0500

    implement delete and post methods

--- src/app/events/event-details/voter.service.ts
+++ src/app/events/event-details/voter.service.ts
@@ -1,17 +1,43 @@
+import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { ISession } from '../shared/event.model';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class VoterService {
-    deleteVoter(session: ISession, voterName: string){
-        session.voters = session.voters.filter(voter => voter !== voterName);
+    constructor(private http: HttpClient) {
     }
 
-    addVoter(session: ISession, voterName: string){
+    deleteVoter(eventId: number, session: ISession, voterName: string){
+        session.voters = session.voters.filter(voter => voter !== voterName);
+
+        const url =`/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+        this.http.delete(url)
+          .pipe(catchError(this.handleError('deleteVoter')))
+          .subscribe();
+    }
+
+    addVoter(eventId: number, session: ISession, voterName: string){
         session.voters.push(voterName);
+
+        const options = { headers: new HttpHeaders ({'Content-Type': '/application/json'})};
+        const url =`/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+        this.http.post(url,{}, options)
+          .pipe(catchError(this.handleError('addVoter')))
+          .subscribe();
     }
 
     userHasVoted(session:ISession, voterName: string){
         return session.voters.some(voter => voter === voterName);
     }
+
+    private handleError<T> (operation = 'operation', result?: T){
+      //return a function takes in type error: any and return Observable<T>
+      //and call console.error and return the result as T
+      return (error: any): Observable<T> => {
+          console.error(error);
+          return of(result as T);
+      }
+  }
 }
